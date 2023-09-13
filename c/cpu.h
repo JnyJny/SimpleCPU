78cee5c2408dd5b8f00a9210c67b2a127022da47
JnyJny <erik.oshaughnessy@gmail.com>
2023-09-13 09:45:13 -0500

    Added instruction_t to the CPU state to hold more info about the instruction in the IR

--- c/cpu.h
+++ c/cpu.h
@@ -3,25 +3,24 @@
 #ifndef USER_H
 #define USER_H
 
+#include "instruction.h"
+
 #define CPU "cpu"
 
-#define DEFAULT_TIMER_INTERVAL 100
-
 typedef struct {
-  int ir;			/* instruction register */
-  int pc;			/* program counter */
-  int sp;			/* stack pointer */
-  int ac;			/* accumulator */
-  int x;			/* operand X */
-  int y;			/* operand Y */
-  int timer_interval;	        /* # cycles to fire timer interrupts */
-  unsigned int mode:1;		/* USER or SYSTEM */
-  unsigned int interrupts:1;	/* 0=off 1=enable */
-  unsigned int debug:1;		/* 0=off 1=enable */
-  unsigned int cycles:29;	/* number of instructions executed */
+  int           ir;			/* instruction register */
+  int           pc;			/* program counter */
+  int           sp;			/* stack pointer */
+  int           ac;			/* accumulator */
+  int           x;			/* operand X */
+  int           y;			/* operand Y */
+  int           timer_interval;	        /* # cycles to fire timer interrupts */
+  unsigned int  mode:1;			/* USER or SYSTEM */
+  unsigned int  interrupts:1;		/* 0=off 1=enable */
+  unsigned int  debug:1;		/* 0=off 1=enable */
+  unsigned int  cycles:29;	        /* number of instructions executed */
+  instruction_t instruction;		/* last instruction decoded */
 } state_t;
-
-
 
 
 #endif	/* USER_H */
