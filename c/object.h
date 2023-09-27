eaa8e636cbc1b08ca38adabaac2401efa1423733
JnyJny <erik.oshaughnessy@gmail.com>
2023-09-27 16:59:11 -0500

    Added symbol support to object.h

--- c/object.h
+++ c/object.h
@@ -7,6 +7,16 @@
 #include "constants.h"
 
 
+typedef struct {
+  word_t  address;
+  char    symbol[SYMSZ];
+} symbol_t;
+
+typedef struct {
+  symbol_t      symtab[NWORDS];
+  instruction_t instructions[NWORDS];
+} program_t;
+
 int read_object_file(int fd, word_t *dst, size_t dstsz);
 int write_object_file(int fd, word_t *src, size_t srcsz);
 
