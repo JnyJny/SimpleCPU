12c39225d0f37a593c77a56f3274e8a5152e018f
JnyJny <erik.oshaughnessy@gmail.com>
2023-10-02 16:23:38 -0500

    Updated test_cpu_sample_programs to take advantage of updated sample.py organization

--- python/tests/test_cpu_sample_programs.py
+++ python/tests/test_cpu_sample_programs.py
@@ -7,44 +7,27 @@ from cpusim.cpu import CPU
 from cpusim.exceptions import *
 from cpusim.asm import Assembler
 
-
-from .program1 import program1
-from .samples import sample1, sample2, sample3, sample4
+from .samples import samples
 
 
 @pytest.mark.parametrize(
-    "source",
+    "source,expected_exception",
     [
-        program1,
+        (samples["program1"][0], None),
+        (samples["sample1"][0], None),
+        (samples["sample2"][0], SegmentationFault),
+        (samples["sample3"][0], None),
+        (samples["sample4"][0], None),
     ],
 )
-def test_assemble_program(source) -> None:
-
-    assembler = Assembler(source.splitlines())
-
-    for address, line in enumerate([line for line in source.splitlines() if len(line)]):
-        value = int(line.split()[0].strip())
-        assert assembler.memory.read(address) == value
-
-
-@pytest.mark.parametrize(
-    "source,error",
-    [
-        (program1, None),
-        (sample1, None),
-        (sample2, SegmentationFault),
-        (sample3, None),
-        (sample4, None),
-    ],
-)
-def test_execute_program(source, error) -> None:
+def test_execute_program(source, expected_exception) -> None:
 
     assembler = Assembler(source.splitlines())
 
     cpu = CPU(assembler.memory)
 
-    if error:
-        with pytest.raises(error):
+    if expected_exception:
+        with pytest.raises(expected_exception):
             cpu.start()
     else:
         cpu.start()
