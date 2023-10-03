0822e00186d45ffaf29f0576c92322e85365f164
JnyJny <erik.oshaughnessy@gmail.com>
2023-10-03 08:21:44 -0500

    Added sample programs A & B

---
+++ docs/program_a.s
@@ -0,0 +1,10 @@
+       8   // Get 
+      14   // CopyToX
+       8   // Get
+      16   // CopyToY
+       8   // Get
+      10   // AddX
+      11   // AddY
+       9   // Put 1
+       1
+      50   // End
---
+++ docs/program_b.s
@@ -0,0 +1,16 @@
+       1   // Load 72=H
+      72
+       9   // Put 2
+       2
+       1   // Load 73=I
+      73
+       9   // Put 2
+       2
+      23   // Call 11
+      11
+      50   // End 
+       1   // Load 10=newline
+      10 
+       9   // Put 2
+       2
+      24   // Return
